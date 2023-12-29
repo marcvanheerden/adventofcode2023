4d4001817a4330f5b7c482b616c3e476120c2aca
marcvanheerden <11750833+marcvanheerden@users.noreply.github.com>
2023-12-30 00:11:25 +0200

    remove comment

--- problems/day25/src/lib.rs
+++ problems/day25/src/lib.rs
@@ -1,19 +1,3 @@
-/*
-jqt: rhn xhk nvd
-rsh: frs pzl lsr
-xhk: hfx
-cmg: qnr nvd lhk bvb
-rhn: xhk bvb hfx
-bvb: xhk hfx
-pzl: lsr hfx nvd
-qnr: nvd
-ntq: jqt hfx bvb xhk
-nvd: lhk
-lsr: lhk
-rzs: qnr cmg lsr rsh
-frs: qnr lhk lsr
-*/
-
 use rand::rngs::StdRng;
 use rand::seq::SliceRandom;
 use rand::SeedableRng;
